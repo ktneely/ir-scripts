fa272300d5d920963a74dbff88f6861a658a2b8b
Kevin Neely <ktneely@astroturfgarden.com>
2015-04-13 15:36:24 -0700

    syncing up

--- VulnMgmt/ov_host-metrics.py
+++ VulnMgmt/ov_host-metrics.py
@@ -10,14 +10,23 @@ import socket
 import xml.etree.ElementTree as ET
 
 #data_dir = 'Directory where Data exists'
-data_dir = '/media/sf_Aruba/InfoSec/Metrics/Data/OpenVAS'
-severity_filter = 6 # arbitrary filter to limit the data
+data_dir = '/media/sf_Aruba/InfoSec/Metrics/Data/OpenVAS/External'
+#severity_filter = 2 # filter the data; 0 = all
+months = ['2014-09', '2014-10', '2014-11', '2015-01', '2015-02']
+severities = {'All':0, 'Low':2, 'Medium':4, 'High':7 }
 # Read the reports
 os.chdir(data_dir)
-reports = glob.glob('report*.xml')
 
-total = 0
-hosts = []
+# Function to address the reports on a monthly basis.  Reports must be
+# named "report-YYYY-MM" until I stop being lazy and fix that issue
+def get_reports(month):
+    period = 'report-' + month + '-*.xml'
+    return glob.glob(period)
+
+# In the future, I need to construct a pd.Dataframe from the data.
+#But that is hard and I don't have the time to do that right now, so
+#brute force, it is!!
+# columns = ['cvss', 'host_ip', 'system_type', 'cve'] #pandas.df cols
 
 def reverse_lookup(ip):
     try:
@@ -27,7 +36,8 @@ def reverse_lookup(ip):
     return hostname
 
 # Process an XML-formatted report
-def parse_report(report):
+def parse_report(report, severity_filter):
+    vulns = 0
     root = ET.parse(report)
     for result in root.findall("./report/results/result"):
     # only process vulnerabilities of a certain severity or higher
@@ -43,21 +53,29 @@ def parse_report(report):
             cvss = result.find('nvt/cvss_base').text
             cve = result.find('nvt/cve').text
             system_type = result.find('nvt/family')
-        # get some additional info based on extracted values
-#            hostname = reverse_lookup(host_ip)  # perform name lookup
-#        impact, urgency, priority = criticality(severity)
-#        category, subcategory = categorize(system_type)
             full_desc = result.find('nvt/tags').text
-            global total; total = total + 1 
-            global hosts; hosts.append(host_ip)
-            print(cvss, host_ip, short_desc)
+            vulns += 1 
+            hosts.append(host_ip)
+#            print(host_ip, short_desc)  # debug
+    return vulns, hosts
+
 
 def uniqify(things):
     return list(set(things))
-                                    
-for report in reports:
-    parse_report(report)
-print("total identified vulns: " + str(total))
-print("total unique hosts: " + str(len(uniqify(hosts))))
+
+for month in months:
+    reports = get_reports(month)
+    print("Reporting period: " + month)
+    for i in severities:
+        total = 0
+        hosts = []
+        print("Severity: " + i)
+        for report in reports:
+            total_add, hosts_add = parse_report(report, severities[i])
+            total += total_add
+            hosts = hosts + hosts_add
+        print("total identified vulns: " + str(total))
+        print("total unique hosts: " + str(len(uniqify(hosts))))
+
 
 
